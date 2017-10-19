cf8ae963b89f31ba8be0d6cf9e87625c7bfba7a2
xiedidan <xiedidan@yeah.net>
2017-10-19 16:46:34 +0800

    Issue #31

---
+++ config/config.js
@@ -0,0 +1,29 @@
+var program = require('commander')
+
+var config = {
+  logLevel: 'info',
+  conalogHost: 'WhoKilledLedCockRobin',
+  conalogPort: 95279528,
+  conalogFrontPort: 9527,
+  mongoUrl: 'mongodb://127.0.0.1:27017/conalog',
+  redisUrl: 'redis://127.0.0.1:6379',
+  activeCollectorPrefix: 'ac_',
+  passiveCollectorPrefix: 'pc_',
+  parseArgs: function() {
+      program
+      .version('1.0.0')
+      .option('--logLevel [logLevel]', 'Log Level [' + this.logLevel + ']', this.logLevel)
+      .option('--redisUrl [redisUrl]', 'Redis URL [' + this.redisUrl + ']', this.redisUrl)
+      .option('--mongoUrl [mongoUrl]', 'MongoDB URL [' + this.mongoUrl + ']', this.mongoUrl)
+      .option('--conalogHost [conalogHost]', 'Conalog Backend Host [' + this.conalogHost + ']', this.conalogHost)
+      .option('--conalogPort [conalogPort]', 'Conalog Backend Port [' + this.conalogPort + ']', this.conalogPort)
+      .option('--activeCollectorPrefix [activeCollectorPrefix]', 'Active Collector Prefix [' + this.activeCollectorPrefix + ']', this.activeCollectorPrefix)
+      .option('--passiveCollectorPrefix [passiveCollectorPrefix]', 'Passive Collector Prefix [' + this.passiveCollectorPrefix + ']', this.passiveCollectorPrefix)
+      .option('--conalogFrontPort [conalogFrontPort]', 'Conalog Frontend Port [' + this.conalogFrontPort + ']', this.conalogFrontPort)
+      .parse(process.argv)
+
+    return program
+  }
+}
+
+module.exports = config;
