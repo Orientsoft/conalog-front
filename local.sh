31358e8c7315021d27092a77fb9f34766b94a963
xiedidan <xiedidan@yeah.net>
2018-03-12 14:21:08 +0800

    update local.sh

--- local.sh
+++ local.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export HOST=192.168.0.48
-export BACKEND_PORT=17527
+export BACKEND_PORT=19527
 sed -i -e 's/WhoKilledLedCockRobin/'$HOST'/g' ./public/js/app/entry.js
 sed -i -e 's/95279528/'$BACKEND_PORT'/g' ./public/js/app/entry.js
 node ./bin/www
